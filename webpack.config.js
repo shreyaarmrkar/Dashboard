6845a82691ed93a9f81c888eb4d454ef5a046018
shreyaarmrkar <shreyaarmarkar@gmail.com>
2023-06-02 11:09:28 +0530

    added dashboard

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "MF-poc",
+    projectName: "MF-POC-dashboard",
+    webpackConfigEnv,
+    argv,
+  });
+
+  const standalonePlugin = defaultConfig.plugins.find(
+    (p) => p.constructor.name === "StandaloneSingleSpaPlugin"
+  );
+
+  standalonePlugin.options.importMapUrl = new URL(
+    "https://react.microfrontends.app/importmap.json"
+  );
+
+  const externals = [/^rxjs\/?.*$/];
+
+  if (webpackConfigEnv.standalone) {
+    externals.push("react", "react-dom");
+  }
+
+  return merge(defaultConfig, {
+    // customizations go here
+    externals,
+  });
+};
